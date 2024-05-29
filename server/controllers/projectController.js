f755e82ac0009d1e0e89916ad03b06c1405134ce
Ante <atokic15@gmail.com>
2024-05-29 16:34:01 +0200

    Add project controllers and routes

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -1,1 +1,21 @@
-ECHO is on.
+const Project = require('../models/Project');
+
+exports.createProject = async (req, res) => {
+  const { name, startDate, endDate } = req.body;
+  try {
+    const newProject = new Project({ name, startDate, endDate });
+    await newProject.save();
+    res.status(201).json(newProject);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.getProjects = async (req, res) => {
+  try {
+    const projects = await Project.find();
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,1 +1,8 @@
-ECHO is on.
+const express = require('express');
+const { createProject, getProjects } = require('../controllers/projectController');
+const router = express.Router();
+
+router.post('/', createProject);
+router.get('/', getProjects);
+
+module.exports = router;
