39abf19fdf8ffbe4783d3ae2c6e0075d4fac0e0f
Ante <atokic15@gmail.com>
2024-05-29 16:32:46 +0200

    Define Project model

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,1 +1,9 @@
-ECHO is on.
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+});
+
+module.exports = mongoose.model('Project', ProjectSchema);
