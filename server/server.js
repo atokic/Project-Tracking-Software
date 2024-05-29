e1abdc6f006bca35dc0a9a8fb975a82cfceaac55
Ante <atokic15@gmail.com>
2024-05-29 16:41:40 +0200

    Complete backend for Project Tracking Software

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config();  // Make sure this line is included
 
 const app = express();
 const PORT = process.env.PORT || 5000;
