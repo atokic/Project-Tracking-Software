4cff0dfd1cfb7fee2fe80efa733c5bd05b787ea6
Ante <atokic15@gmail.com>
2024-05-30 00:26:26 +0200

    Pagination of the projects grid

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -12,9 +12,18 @@ exports.createProject = async (req, res) => {
 };
 
 exports.getProjects = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = 5; // Number of projects per page
+
   try {
-    const projects = await Project.find();
-    res.status(200).json(projects);
+    const totalProjects = await Project.countDocuments();
+    const totalPages = Math.ceil(totalProjects / pageSize);
+
+    const projects = await Project.find()
+      .skip((page - 1) * pageSize)
+      .limit(pageSize);
+
+    res.status(200).json({ projects, totalPages });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -4,5 +4,5 @@ const router = express.Router();
 
 router.post('/', createProject);
 router.get('/', getProjects);
-
+  
 module.exports = router;
